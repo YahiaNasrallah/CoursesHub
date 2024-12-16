0ce5fb7c2f9eb308a2cf5355c03dc929932ad50d
YahiaNasrallah <126978943+YahiaNasrallah@users.noreply.github.com>
2024-12-16 23:29:23 +0200

    DashboardActivity

--- app/src/main/java/com/example/coursesapp/DashboardActivity.java
+++ app/src/main/java/com/example/coursesapp/DashboardActivity.java
@@ -1,24 +1,65 @@
 package com.example.coursesapp;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.coursesapp.databinding.ActivityDashboardBinding;
+
 public class DashboardActivity extends AppCompatActivity {
 
+    ActivityDashboardBinding binding;
+    Appdatabase db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_dashboard);
+        binding = ActivityDashboardBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        db=Appdatabase.getDatabase(this);
+
+        View coustem= LayoutInflater.from(DashboardActivity.this).inflate(R.layout.log,null);
+        AlertDialog.Builder bulder=new AlertDialog.Builder(this);
+        bulder.setView(coustem);
+        EditText ed_category_name=coustem.findViewById(R.id.ed_category_name);
+        Button btn_ok=coustem.findViewById(R.id.btn_ok);
+
+
+
+
+        AlertDialog dialog=bulder.create();
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.setCancelable(false);
+        dialog.show();
+
+        btn_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                db.categoryDao().insertCategory(new Category(ed_category_name.getText().toString()));
+                dialog.hide();
+
+            }
+        });
+
+
+
+
     }
 }
